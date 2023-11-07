f10496202570bed5de9f0c81d9db7fc95d92b73e
rousseaux diego <diego.rousseaux@student.vinci.be>
2023-11-07 15:15:09 +0100

    board +select+basicmove

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,14 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import ChessPage from '../Pages/Chess';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/new': NewPage,
+  '/chess': ChessPage,
+
 };
 
 export default routes;
