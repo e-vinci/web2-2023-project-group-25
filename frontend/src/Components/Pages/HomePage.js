5b40b5200b8b52798330a6a95341ddc91c38baee
ali beztout <ali.beztout@student.vinci.be>
2023-11-28 15:17:08 +0100

    refactoring image

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -12,37 +12,36 @@ const HomePage = () => {
 
   
   const homePageContent = `
-  
-  <div class="container text-center no-scroll">
-    <div class="row pb-5">
-      <div class="col">
-        <h3 class="display-4">Bienvenue sur Chess Rumble !</h3>
-        <p class="lead">Amusez-vous ;)</p>
+    <div class="container text-center no-scroll">
+      <div class="row pb-5">
+        <div class="col">
+          <h3 class="display-4">Bienvenue sur Chess Rumble !</h3>
+          <p class="lead">Amusez-vous ;)</p>
+        </div>
+      </div>
+
+      <div class="row pb-5">
+        <div class="col">
+          <div id="groot">
+            <img class="chessImage" src="${chessImage}" alt="chessImage" />
+          </div>
+        </div>
+      </div>
+
+      <div class="row pb-5">
+        <div class="col">
+          <button id="playButton" class="btn btn-primary btn-lg custom-btn">Play</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col history-section">
+          <h3>Historique des parties</h3>
+          <p>Consultez vos parties.</p>
+        </div>
       </div>
     </div>
-
-          <div class="row">
-            <div id="groot" class="col-12 col-lg-6 clickable">
-              <img class="chessImage" src="${chessImage}" alt="chessImage" />
-            </div>
-            </div>
-    
-
-   <div class="row pb-5">
-      <div class="col">
-      <button id="playButton" class="btn btn-primary btn-lg custom-btn">Play</button>
-       </div>
-    </div>
-
-    <div class="row">
-      <div class="col history-section">
-        <h3>Historique des parties</h3>
-        <p>Consultez vos parties.</p>
-      </div>
-    </div>
-  </div>
-  
-`;
+  `;
 
 
 
